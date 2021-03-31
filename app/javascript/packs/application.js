360c3527e5c4a4ba0aa3848e19fe9da53ee8805c
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-03-31 22:13:31 +0900

    仮モーダルの設置

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import 'bootstrap';
+import '../stylesheets/application';
 
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
