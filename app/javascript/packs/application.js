81366f1f3849ec189e6276df350d736e0db96831
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-03-31 20:32:29 +0900

    turbolinksnの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,9 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("../calendar")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +14,16 @@ require("../calendar")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+window.onload = function () {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin, interactionPlugin]
+  });
+  calendar.render();
+}
