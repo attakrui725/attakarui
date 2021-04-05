6e1a278b5836ba4b8ab5d593db19af28f87d45e1
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-04-05 21:35:06 +0900

    テンプレート画像表示のリファクタリング

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ApplicationRecord
+  TEMPLATES = {
+    bath_cleaning: '風呂掃除',
+    checking_post: '郵便の確認',
+    cooking_papa: '朝ごはんをつくる',
+    garvage_removal: 'ゴミ出し',
+    ready_to_go_to_kindergarten: '保育園の準備',
+    toilet_cleaning: 'トイレ掃除',
+    walking_dog: '犬の散歩',
+    washing_clothes: '洗濯',
+  }
   has_one_attached :image
 end
