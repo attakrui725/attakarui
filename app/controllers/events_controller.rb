59aa0557cd726317cbd5fa5da0395adb447cc5d8
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-04-05 15:55:54 +0900

    active strageの実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -47,6 +47,6 @@ class EventsController < ApplicationController
 
   private
   def event_params
-    params.require(:event).permit(:title, :start_at)
+    params.require(:event).permit(:title, :start_at, :image)
   end
 end
